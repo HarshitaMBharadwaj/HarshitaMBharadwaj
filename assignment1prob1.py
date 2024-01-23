e47be0215cfa955e72df8f7c72e0a13b06de2de2
HarshitaMBharadwaj <85123203+HarshitaMBharadwaj@users.noreply.github.com>
2024-01-23 11:03:43 -0500

    Delete assignment1prob1.py

--- assignment1prob1.py
+++
@@ -1,8 +0,0 @@
-#simple intrest
-
-p=float(input("enter the principle amount:"))
-t=float(input("enter the time :"))
-r=float(input("enter the rate:"))
-
-si=(p*t*r)/100
-print("the simple intrest:",si)
